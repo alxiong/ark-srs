434bf4087b014ab3fe06f1edf4a0b9e7b90563f0
Alex Xiong <alex.xiong.tech@gmail.com>
2023-11-09 17:29:07 +0800

    feat: upgrade dep to arkwork 0.4.0

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Use Common/Structured Reference String (CRS/SRS) from existing ceremonies with e
 
 **WARNING: This is work in progress, none of the code has been audited. The library is NOT ready for production.**
 
+## Download SRS to local
+
+- Aztec's ignition: `./scripts/download_transcripts_aztec.sh NUM` where `NUM` can be `0..19` (`NUM=2` means download transcript `0, 1, 2`)
+  - 100.8 million BN254 G1 points in total, split up into 20 files, each transcript file contains ~5 million points (~307 MB in size)
+  - 2 BN254 G2 points are in the first transcript file
+  - For common use cases, we only need degrees of less than `2^17`. We have provided smaller arkwork-serializable binary files containing fewer power-of-two
+  degrees SRS and loading functions in the library so you can easily plug in (or copy-paste over) into your code.
+
 ## Usage
 
 ```rust
@@ -12,14 +20,7 @@ use ark_poly::univariate::DensePolynomial;
 
 // simulated CRS (for test only)
 let pp = KZG10::<Bn254, DensePolynomial<<Bn254 as PairingEngine>::Fr>>::setup(max_degree, false, &mut rng)?;
-// then pick the `supported_degree` <= `max_degree`
-let (commit_key, ver_key) = trim(&pp, supported_degree)?;
 
 // now, use Aztec's CRS
-let (ck, vk) = use crs::aztec20::kzg10_setup(supported_degree)?;
-
-// e.g. in Plonk, you would use:
-// `domain_size` is the evaluation domain size for FFT.
-// `2` is the degree for masking polynomial to achieve ZK.
-let (ck, vk) = use crs::aztec20::kzg10_setup(domain_size + 2)?;
+let pp = use crs::aztec20::kzg10_setup(supported_degree)?;
 ```
--- src/aztec20.rs
+++ src/aztec20.rs
@@ -3,9 +3,9 @@
 
 use anyhow::{bail, Result};
 use ark_bn254::{Bn254, Fq, Fq2, G1Affine, G2Affine};
-use ark_ec::AffineCurve;
+use ark_ec::AffineRepr;
 use ark_ff::{BigInteger256, PrimeField};
-use ark_poly_commit::kzg10::{Powers, UniversalParams, VerifierKey};
+use ark_poly_commit::kzg10::UniversalParams;
 use ark_std::{
     collections::BTreeMap,
     format,
@@ -15,7 +15,7 @@ use ark_std::{
     vec::Vec,
 };
 
-const TRANSCRIPT_DIR: &'static str = "./data/aztec20";
+const TRANSCRIPT_DIR: &str = "./data/aztec20";
 const NUM_TRANSCRIPTS: usize = 20;
 const NUM_G1_PER_TRANSCRIPT: usize = 5_040_000;
 const NUM_G2: usize = 2;
@@ -27,15 +27,13 @@ const NUM_BIGINT_PER_G2: usize = 4;
 /// the polynomial commitment scheme.
 /// This API is similar to [KZG10::setup][setup]
 ///
-/// [setup]: https://docs.rs/ark-poly-commit/0.3.0/ark_poly_commit/kzg10/struct.KZG10.html#method.setup
-pub fn kzg10_setup(
-    supported_degree: usize,
-) -> Result<(Powers<'static, Bn254>, VerifierKey<Bn254>)> {
-    if supported_degree < 1 || supported_degree > NUM_G1_PER_TRANSCRIPT * NUM_TRANSCRIPTS {
+/// [setup]: https://docs.rs/ark-poly-commit/0.4.0/ark_poly_commit/kzg10/struct.KZG10.html#method.setup
+pub fn kzg10_setup(supported_degree: usize) -> Result<UniversalParams<Bn254>> {
+    if !(1..=NUM_G1_PER_TRANSCRIPT * NUM_TRANSCRIPTS).contains(&supported_degree) {
         bail!("Max degree has to be between [1, 100.8 million].");
     }
 
-    let mut powers_of_g = vec![G1Affine::prime_subgroup_generator()];
+    let mut powers_of_g = vec![G1Affine::generator()];
     powers_of_g.extend_from_slice(&parse_g1_points(supported_degree)?);
 
     // NOTE: used for hiding variant of KZG, not supported in Aztec's CRS.
@@ -43,15 +41,10 @@ pub fn kzg10_setup(
     // NOTE: not supported in Aztec's CRS.
     let neg_powers_of_h = BTreeMap::new();
 
-    let h = G2Affine::prime_subgroup_generator();
+    let h = G2Affine::generator();
     let [beta_h, _] = parse_g2_points()?;
     let prepared_h = h.into();
     let prepared_beta_h = beta_h.into();
-
-    let powers = Powers {
-        powers_of_g: ark_std::borrow::Cow::Owned(powers_of_g.clone()),
-        powers_of_gamma_g: ark_std::borrow::Cow::Owned(vec![]), // empty, not used
-    };
 
     let pp: UniversalParams<Bn254> = UniversalParams {
         powers_of_g,
@@ -62,17 +55,7 @@ pub fn kzg10_setup(
         prepared_h,
         prepared_beta_h,
     };
-
-    let vk = VerifierKey {
-        g: pp.powers_of_g[0],
-        gamma_g: ark_bn254::G1Affine::default(), // dummy gamma_g, not used
-        h: pp.h,
-        beta_h: pp.beta_h,
-        prepared_h: pp.prepared_h.clone(),
-        prepared_beta_h: pp.prepared_beta_h.clone(),
-    };
-
-    Ok((powers, vk))
+    Ok(pp)
 }
 
 // Returns x.[1], x^2.[1], ... , x^`bound`.[1] where `x` is toxic
@@ -102,6 +85,7 @@ fn parse_g1_points(bound: usize) -> Result<Vec<G1Affine>> {
 // Parse G1Affine points from CRS
 // Concrete format spec:
 // https://github.com/AztecProtocol/ignition-verification/blob/master/Transcript_spec.md#structure-of-a-transcript-file
+#[allow(clippy::needless_range_loop)]
 fn parse_g1_points_from_file(f: &mut File, num_points: usize) -> Result<Vec<G1Affine>> {
     let mut g1_points = Vec::new();
 
@@ -113,26 +97,25 @@ fn parse_g1_points_from_file(f: &mut File, num_points: usize) -> Result<Vec<G1Af
         // [X, Y]
         let mut bigints_repr = [[0u64; 4]; NUM_BIGINT_PER_G1];
         for j in 0..NUM_BIGINT_PER_G1 {
-            for k in 0..4 {
+            for k in 0usize..4 {
                 // size of G1 is 64
                 // size of a bigint is 32 bytes.
                 // size of u64 is 8 bytes.
                 f.seek(SeekFrom::Start(
-                    G1_STARTING_POS + i as u64 * 64 + j as u64 * 32 + k * 8,
+                    G1_STARTING_POS + i as u64 * 64 + j as u64 * 32 + k as u64 * 8,
                 ))?;
                 let mut buf = [0u8; 8];
                 f.read_exact(&mut buf)?;
-                bigints_repr[j as usize][k as usize] = u64::from_be_bytes(buf);
+                bigints_repr[j][k] = u64::from_be_bytes(buf);
             }
         }
 
-        let x = Fq::from_repr(BigInteger256::new(bigints_repr[0]))
+        let x = Fq::from_bigint(BigInteger256::new(bigints_repr[0]))
             .expect("Failed to parse G1 point's x-coordinate");
-        let y = Fq::from_repr(BigInteger256::new(bigints_repr[1]))
+        let y = Fq::from_bigint(BigInteger256::new(bigints_repr[1]))
             .expect("Failed to parse G1 point's y-coordinate");
 
-        let point = G1Affine::new(x, y, false);
-        assert!(point.is_on_curve());
+        let point = G1Affine::new(x, y); // point.is_on_curve() already checked during `new()`
         g1_points.push(point);
     }
     Ok(g1_points)
@@ -143,6 +126,7 @@ fn parse_g1_points_from_file(f: &mut File, num_points: usize) -> Result<Vec<G1Af
 // https://github.com/AztecProtocol/ignition-verification/blob/master/Transcript_spec.md#structure-of-a-transcript-file
 // NOTE: the second G2 point is not used in CRS, but only for transcript
 // verification purposes.
+#[allow(clippy::needless_range_loop)]
 fn parse_g2_points() -> Result<[G2Affine; NUM_G2]> {
     let mut g2_points = [G2Affine::default(); NUM_G2];
 
@@ -153,7 +137,7 @@ fn parse_g2_points() -> Result<[G2Affine; NUM_G2]> {
         let mut bigints_repr = [[0u64; 4]; NUM_BIGINT_PER_G2];
 
         for j in 0..NUM_BIGINT_PER_G2 {
-            for k in 0..4 {
+            for k in 0usize..4 {
                 // size of G1 is 64
                 // size of G2 is 128 bytes (4 * 32).
                 // size of a bigint is 32 bytes.
@@ -163,25 +147,25 @@ fn parse_g2_points() -> Result<[G2Affine; NUM_G2]> {
                         + NUM_G1_PER_TRANSCRIPT as u64 * 64
                         + i as u64 * 128
                         + j as u64 * 32
-                        + k * 8,
+                        + k as u64 * 8,
                 ))?;
                 let mut buf = [0u8; 8];
                 f.read_exact(&mut buf)?;
-                bigints_repr[j as usize][k as usize] = u64::from_be_bytes(buf);
+                bigints_repr[j][k] = u64::from_be_bytes(buf);
             }
         }
-        let x_c0 = Fq::from_repr(BigInteger256::new(bigints_repr[0]))
+        let x_c0 = Fq::from_bigint(BigInteger256::new(bigints_repr[0]))
             .expect("Failed to parse G2 point's X.c0");
-        let x_c1 = Fq::from_repr(BigInteger256::new(bigints_repr[1]))
+        let x_c1 = Fq::from_bigint(BigInteger256::new(bigints_repr[1]))
             .expect("Failed to parse G2 point's X.c1");
-        let y_c0 = Fq::from_repr(BigInteger256::new(bigints_repr[2]))
+        let y_c0 = Fq::from_bigint(BigInteger256::new(bigints_repr[2]))
             .expect("Failed to parse G2 point's Y.c0");
-        let y_c1 = Fq::from_repr(BigInteger256::new(bigints_repr[3]))
+        let y_c1 = Fq::from_bigint(BigInteger256::new(bigints_repr[3]))
             .expect("Failed to parse G2 point's Y.c1");
         let x = Fq2::new(x_c0, x_c1);
         let y = Fq2::new(y_c0, y_c1);
 
-        let point = G2Affine::new(x, y, false);
+        let point = G2Affine::new(x, y);
         assert!(point.is_on_curve());
         g2_points[i] = point;
     }
@@ -192,12 +176,12 @@ fn parse_g2_points() -> Result<[G2Affine; NUM_G2]> {
 #[cfg(test)]
 mod test {
     use super::*;
-    use ark_ec::{msm::VariableBaseMSM, PairingEngine, ProjectiveCurve};
+    use ark_ec::{pairing::Pairing, CurveGroup, VariableBaseMSM};
     use ark_ff::UniformRand;
-    use ark_poly::{univariate::DensePolynomial, Polynomial};
+    use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial, Polynomial};
     use ark_poly_commit::{
-        kzg10::{Powers, Proof, Randomness, KZG10},
-        PCRandomness, UVPolynomial,
+        kzg10::{Powers, Proof, Randomness, VerifierKey, KZG10},
+        PCRandomness,
     };
     use ark_std::ops::Div;
 
@@ -206,11 +190,11 @@ mod test {
         powers: &Powers<E>,
         p: &P,
         point: P::Point,
-        rand: &Randomness<E::Fr, P>,
+        rand: &Randomness<E::ScalarField, P>,
     ) -> Result<Proof<E>>
     where
-        E: PairingEngine,
-        P: UVPolynomial<E::Fr, Point = E::Fr>,
+        E: Pairing,
+        P: DenseUVPolynomial<E::ScalarField, Point = E::ScalarField>,
         for<'a, 'b> &'a P: Div<&'b P, Output = P>,
     {
         // check_degree_is_too_large()
@@ -228,7 +212,7 @@ mod test {
         let (num_leading_zeros, witness_coeffs) =
             skip_leading_zeros_and_convert_to_bigints(&witness_poly);
 
-        let w = VariableBaseMSM::multi_scalar_mul(
+        let w = <E::G1 as VariableBaseMSM>::msm_bigint(
             &powers.powers_of_g[num_leading_zeros..],
             &witness_coeffs,
         );
@@ -238,7 +222,34 @@ mod test {
         })
     }
 
-    fn skip_leading_zeros_and_convert_to_bigints<F: PrimeField, P: UVPolynomial<F>>(
+    /// Specializes the public parameters for a given maximum degree `d` for
+    /// polynomials `d` should be less that `pp.max_degree()`.
+    /// Modify from <https://github.com/arkworks-rs/poly-commit/blob/master/poly-commit/src/kzg10/mod.rs#L512>
+    fn trim<E: Pairing>(
+        pp: &UniversalParams<E>,
+        mut supported_degree: usize,
+    ) -> Result<(Powers<E>, VerifierKey<E>)> {
+        if supported_degree == 1 {
+            supported_degree += 1;
+        }
+        let powers_of_g = pp.powers_of_g[..=supported_degree].to_vec();
+
+        let powers = Powers {
+            powers_of_g: ark_std::borrow::Cow::Owned(powers_of_g),
+            powers_of_gamma_g: ark_std::borrow::Cow::Owned(vec![]), // empty, not available
+        };
+        let vk = VerifierKey {
+            g: pp.powers_of_g[0],
+            gamma_g: E::G1Affine::default(), // dummy gamma_g, not available
+            h: pp.h,
+            beta_h: pp.beta_h,
+            prepared_h: pp.prepared_h.clone(),
+            prepared_beta_h: pp.prepared_beta_h.clone(),
+        };
+        Ok((powers, vk))
+    }
+
+    fn skip_leading_zeros_and_convert_to_bigints<F: PrimeField, P: DenseUVPolynomial<F>>(
         p: &P,
     ) -> (usize, Vec<F::BigInt>) {
         let mut num_leading_zeros = 0;
@@ -246,7 +257,7 @@ mod test {
             num_leading_zeros += 1;
         }
         let coeffs = ark_std::cfg_iter!(&p.coeffs()[num_leading_zeros..])
-            .map(|s| s.into_repr())
+            .map(|s| s.into_bigint())
             .collect::<Vec<_>>();
         (num_leading_zeros, coeffs)
     }
@@ -261,8 +272,9 @@ mod test {
             while degree <= 1 {
                 degree = usize::rand(rng) % 2usize.pow(16);
             }
-            let (ck, vk) = kzg10_setup(degree)?;
-            let p: DensePolynomial<ark_bn254::Fr> = UVPolynomial::rand(degree, rng);
+            let srs = kzg10_setup(degree)?;
+            let (ck, vk) = trim(&srs, degree)?;
+            let p: DensePolynomial<ark_bn254::Fr> = DenseUVPolynomial::rand(degree, rng);
             let (comm, rand) = KZG10::commit(&ck, &p, None, None)?;
             let point = ark_bn254::Fr::rand(rng);
             let value = p.evaluate(&point);
